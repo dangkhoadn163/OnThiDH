bbce833b638eaf38cbebab94514f5e8861efcc6a
Dang Quoc Huy <cubo2411vnn@gmail.com>
2017-11-08 01:52:53 +0700

    Kiểm tra image khi load xong

--- app/src/main/java/com/example/dk/onthidh/Activity/Test.java
+++ app/src/main/java/com/example/dk/onthidh/Activity/Test.java
@@ -35,7 +35,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.squareup.picasso.Picasso;
+
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
--- app/src/main/java/com/example/dk/onthidh/FolderMoi/MoiAdapter.java
+++ app/src/main/java/com/example/dk/onthidh/FolderMoi/MoiAdapter.java
@@ -1,14 +1,12 @@
 package com.example.dk.onthidh.FolderMoi;
 
 import android.content.Context;
-
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-
 
 import com.example.dk.onthidh.R;
 import com.squareup.picasso.Picasso;
