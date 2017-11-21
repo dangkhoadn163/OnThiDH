1fdf7cfd0dbb5b9123f63b6a5786cd01c95fb032
Dang Quoc Huy <cubo2411vnn@gmail.com>
2017-11-21 11:54:44 +0700

    Cập nhật Search version: 3.6(Fix một số lỗi nhỏ)

--- app/src/main/java/com/example/dk/onthidh/Activity/ListTest.java
+++ app/src/main/java/com/example/dk/onthidh/Activity/ListTest.java
@@ -24,6 +24,9 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
 import java.text.Normalizer;
 import java.util.ArrayList;
 
@@ -135,6 +138,21 @@ public class ListTest extends AppCompatActivity {
         return Normalizer.normalize(string, Normalizer.Form.NFD)
                 .replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
     }
+
+    public static String flattenToAscii(String string) {
+        StringBuilder sb = new StringBuilder(string.length());
+        string = Normalizer.normalize(string, Normalizer.Form.NFD);
+        for (char c : string.toCharArray()) {
+            if (c > '\u007F')
+            {
+                sb.append(c);
+                Log.d("char", c + "");
+            }
+               //sb.append(c);
+
+        }
+        return sb.toString();
+    }
     private void loadAll(final String nameTest, final ArrayList<MyFile> newList)
     {
         DatabaseReference ref1 = FirebaseDatabase.getInstance().getReference();
@@ -146,18 +164,77 @@ public class ListTest extends AppCompatActivity {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // Result will be holded Here
                 for (final DataSnapshot dsp : dataSnapshot.getChildren()) {
+                    MyFile model = new MyFile();
                     String temp = dsp.child("text").getValue().toString().toLowerCase();
                     String temp2 = nameTest.toLowerCase();
                     String comparetemp = removeDiacriticalMarks(temp);
                     String comparetemp2 = removeDiacriticalMarks(temp2);
 
-                    MyFile model = new MyFile();
-                    if (temp.contains(temp2) || (comparetemp.contains(comparetemp2) && temp2.contains(comparetemp2)))
+                    String accentTemp2 = flattenToAscii(temp2).toString();
+                    int nTemp2 = accentTemp2.length();
+                    boolean check = false;
+
+                    String replaceD = comparetemp.replaceAll("đ", "d");
+                    String replaceD2 = comparetemp2.replaceAll("đ", "d");
+                    Log.d("huy", replaceD + "");
+                    if(replaceD.contains(replaceD2) && nTemp2 != 0 && !check)
                     {
-                        Log.d("huy", model.text + "");
-                        Log.d("huyabc", newList.size() + "");
-                        Log.d("huyxyz", model.text + "");
-                        Log.d("getkey", dsp.getKey());
+                        int pos = replaceD.indexOf(replaceD2);
+                        String sb = temp.substring(pos, temp2.length() + pos);
+                        String accentTemp = flattenToAscii(sb).toString();
+                        int nTemp = accentTemp.length();
+                        Log.d("sb", sb);
+                        Log.d("accentTemp", accentTemp);
+                        Log.d("accentTemp2", accentTemp2);
+                        if(accentTemp.contains(accentTemp2) && !check)
+                        {
+                            check = true;
+                            model.text = dsp.child("text").getValue().toString();
+                            newList.add(model);
+                            adapter.notifyDataSetChanged();
+                            adapter.setOnItemClickListener(new MyFileAdapter.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(View view, int position) {
+                                    Intent intent= new Intent(ListTest.this,Test.class);
+                                    intent.putExtra("keyt",dsp.getRef().getKey());
+                                    intent.putExtra("Uid2", uid);
+                                    intent.putExtra("monhoc",monhoc);
+                                    ListTest.this.startActivity(intent);
+                                }
+                            });
+                        }
+                        else if(!accentTemp.contains(accentTemp2) && !check)
+                        {
+                            //int length = nTemp2 < nTemp ? nTemp2 : nTemp;
+                            accentTemp = accentTemp.replaceAll("̉đ", "đ");
+                            for(int i = 0; i < nTemp2; i++)
+                            {
+                                for(int j = 0; j < nTemp; j++)
+                                {
+                                    if (accentTemp2.charAt(i) == accentTemp.charAt(j) && !check)
+                                    {
+                                        check = true;
+                                        model.text = dsp.child("text").getValue().toString();
+                                        newList.add(model);
+                                        adapter.notifyDataSetChanged();
+                                        adapter.setOnItemClickListener(new MyFileAdapter.OnItemClickListener() {
+                                            @Override
+                                            public void onItemClick(View view, int position) {
+                                                Intent intent = new Intent(ListTest.this, Test.class);
+                                                intent.putExtra("keyt", dsp.getRef().getKey());
+                                                intent.putExtra("Uid2", uid);
+                                                intent.putExtra("monhoc", monhoc);
+                                                ListTest.this.startActivity(intent);
+                                            }
+                                        });
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if ((temp.contains(temp2) || (replaceD.contains(replaceD2) && temp2.contains(replaceD2))) && !check)
+                    {
                         model.text = dsp.child("text").getValue().toString();
                         newList.add(model);
                         adapter.notifyDataSetChanged();
